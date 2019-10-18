1bfc2c89ab5a8a8f507e20c9509122e159b5ce3b
Olgerd_Seviakov <olgerd@zenitech.co.uk>
2019-10-18 10:50:33 +0300

    remove front-end

--- client/src/config/endpoints.ts
+++
@@ -1,2 +0,0 @@
-const baseUrl = 'http://localhost:5000';
-export const helloWorldApi = baseUrl + '/helloWorld';
--- client/src/rxjs-as-redux/storeInstances.ts
+++
@@ -1,7 +0,0 @@
-import { createStore } from './RxStore';
-import userReducer from '../pages/user-page/userReducer';
-import React from 'react';
-
-const initState = { name: 'Harry', isLoading: false, users: [] };
-export const userStore = createStore(initState, userReducer);
-export const StateContext = React.createContext(initState);
