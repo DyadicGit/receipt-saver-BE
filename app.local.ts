9d4ab896bc4b7039c50530c501483c8df51495d3
Olgerd_Seviakov <olgerd@zenitech.co.uk>
2019-10-22 17:45:27 +0300

    manage env variables from one file

--- app.local.ts
+++ app.local.ts
@@ -1,8 +1,8 @@
 const app = require('./app');
-import { CLIENT_PORT, SERVER_PORT } from './config/default_env.json';
+import { CLIENT_URL, SERVER_PORT } from './config/default_env.json';
 
 app.listen(SERVER_PORT, () => {
   console.log(
-    `server: listening on port ${SERVER_PORT}, Front-end cors accessible with port ${CLIENT_PORT}"`
+    `server: listening on port ${SERVER_PORT}, Front-end cors accessible with port ${CLIENT_URL}"`
   );
 });
