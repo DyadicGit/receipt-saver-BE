ff7604419670edeecb3af6d3b67636b1ee2438bd
Olgerd_Seviakov <olgerd@zenitech.co.uk>
2019-11-11 11:27:10 +0200

    CORS headers update

--- app.ts
+++ app.ts
@@ -21,6 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', CLIENT_URL);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
   next();
 });
 
